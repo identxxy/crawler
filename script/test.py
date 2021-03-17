173a61852b7bd3bce9cfe65c2dbb33f26e9bf29d
vox09 <xuxinyuan09@outlook.com>
2021-03-18 00:41:02 +0800

    final multiple launch

--- script/test.py
+++ script/test.py
@@ -4,7 +4,7 @@ import crawler.register_all_env
 import rospy
 
 rospy.init_node('test')
-env = gym.make('CrawlerWalkXEnv-v0', robot_id=0)
-env = gym.make('CrawlerWalkXEnv-v0', robot_id=1)
+env1 = gym.make('CrawlerWalkXEnv-v0', robot_id=0)
+env2 = gym.make('CrawlerWalkXEnv-v0', robot_id=1)
 env.reset()
 
