31c7eefabe7af3caadaadef3b3248497bdd3a821
vox09 <xuxinyuan09@outlook.com>
2021-04-04 01:41:48 +0800

    add abs

--- openai_ros/crawler/walk_forward_cam.py
+++ openai_ros/crawler/walk_forward_cam.py
@@ -165,7 +165,7 @@ class WalkXTaskEnv_v1(WalkXCamTaskEnv):
             y = r.global_pos.position.y
             lastx = self.lastPos[0,i]
             lasty = self.lastPos[1,i]
-            reward = ( (x-lastx) * self.reward_x_vel + (y-lasty) * self.reward_y_vel ) / self.running_step
+            reward = ( (x-lastx) * self.reward_x_vel + abs(y-lasty) * self.reward_y_vel ) / self.running_step
             reward -= self.reward_ori_k * ( 1 - math.cos(r.roll) * math.cos(r.pitch) )
             if r.global_pos.position.z < self.reward_height_thd: # punishment
                 reward += self.reward_height_k * (r.global_pos.position.z - self.reward_height_thd)
